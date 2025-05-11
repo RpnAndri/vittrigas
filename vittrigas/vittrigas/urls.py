9c182603d31ece1e29f3bc616c3f81d54bdbe872
RpnAndri <andri.crisostomo@gmail.com>
2025-05-11 22:10:48 +0900

    fix: center navbar

--- vittrigas/vittrigas/urls.py
+++ vittrigas/vittrigas/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . views import Index
+from . views import Index, SignUp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
+    path('signup/', SignUp.as_view(), name='signup'),
 ]
--- vittrigas/vittrigas/views.py
+++ vittrigas/vittrigas/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+
+from . forms import UserCreateForm
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+class SignUp(CreateView):
+    form_class = UserCreateForm
+    success_url = reverse_lazy('index')
+    template_name = 'accounts/signup.html'
