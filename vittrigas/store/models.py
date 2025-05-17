a060ea87d43390f0eda58540dcd2afefc0cd275d
MarcelTreptow <trepma01@thu.de>
2025-05-17 15:00:04 +0900

    Add untested cart functionality

--- vittrigas/store/models.py
+++ vittrigas/store/models.py
@@ -13,3 +13,22 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
--- vittrigas/store/views.py
+++ vittrigas/store/views.py
@@ -7,3 +7,22 @@ class Product(ListView):
     model = models.Product
     template_name = 'product_list.html'
     context_object_name = 'products' 
+
+#@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    customer = get_object_or_404(Customer, user=request.user)
+    cart, created = Cart.objects.get_or_create(customer=customer)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('cart_detail')
+
+#@login_required
+def cart_detail(request):
+    customer = get_object_or_404(Customer, user=request.user)
+    cart, created = Cart.objects.get_or_create(customer=customer)
+    return render(request, 'cart_detail.html', {'cart': cart})
