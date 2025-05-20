a19696e0d1f0f4f5c3b62e7178022dd9817b91b2
RpnAndri <andri.crisostomo@gmail.com>
2025-05-20 20:17:02 +0900

    edit: navbar side

--- vittrigas/static/js/base.js
+++ vittrigas/static/js/base.js
@@ -1,10 +1,12 @@
 hamburger = document.getElementsByClassName("hamburger")[0];
 nav_pop = document.getElementsByClassName("nav-pop")[0];
 line_one = document.getElementsByClassName("line1")[0];
+line_two = document.getElementsByClassName("line2")[0];
 line_three = document.getElementsByClassName("line3")[0];
 
 hamburger.addEventListener("click", () => {
     nav_pop.classList.toggle('active');
     line_one.classList.toggle('bend');
+    line_two.classList.toggle('bend');
     line_three.classList.toggle('bend');
 });
