ff4196b73d3b133daded9d1a8c2256cac531c9cb
RpnAndri <andri.crisostomo@gmail.com>
2025-05-20 17:34:40 +0900

    edit: navbar media queries

---
+++ vittrigas/static/js/base.js
@@ -0,0 +1,10 @@
+hamburger = document.getElementsByClassName("hamburger")[0];
+nav_pop = document.getElementsByClassName("nav-pop")[0];
+line_one = document.getElementsByClassName("line1")[0];
+line_three = document.getElementsByClassName("line3")[0];
+
+hamburger.addEventListener("click", () => {
+    nav_pop.classList.toggle('active');
+    line_one.classList.toggle('bend');
+    line_three.classList.toggle('bend');
+});
