75a3b88a96942a777a8984fad1c6dcecc1014232
jotarrobles <juanroblesangulo@gmail.com>
2025-06-02 20:26:32 +0900

    Fixing links in hp

--- vittrigas/static/js/base.js
+++ vittrigas/static/js/base.js
@@ -1,15 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+hamburger = document.getElementById("hamburger");
+nav_pop = document.getElementsByClassName("nav-pop")[0];
+line_one = document.getElementsByClassName("line1")[0];
+line_two = document.getElementsByClassName("line2")[0];
+line_three = document.getElementsByClassName("line3")[0];
 
-  hamburger = document.getElementById("hamburger");
-  nav_pop = document.getElementsByClassName("nav-pop")[0];
-  line_one = document.getElementsByClassName("line1")[0];
-  line_two = document.getElementsByClassName("line2")[0];
-  line_three = document.getElementsByClassName("line3")[0];
-
-  hamburger.addEventListener("click", () => {
-      nav_pop.classList.toggle('active');
-      line_one.classList.toggle('bend');
-      line_two.classList.toggle('bend');
-      line_three.classList.toggle('bend');
-  });
+hamburger.addEventListener("click", () => {
+    nav_pop.classList.toggle('active');
+    line_one.classList.toggle('bend');
+    line_two.classList.toggle('bend');
+    line_three.classList.toggle('bend');
 });
