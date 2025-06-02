f6750b14a7cab0b42e7f8533ea30b8ad37745dfd
MarcelTreptow <trepma01@thu.de>
2025-06-02 15:39:57 +0900

    clean up views.py

--- vittrigas/store/views.py
+++ vittrigas/store/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
-from .models import Product, Customer, Cart, CartItem
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.db.models import Sum
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-# Create your views here.
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Product, Customer, Cart, CartItem
+
 
 class ProductListView(ListView):
     model = Product
@@ -18,36 +16,37 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         customer = get_object_or_404(Customer, user=self.request.user)
-        cart, created = Cart.objects.get_or_create(customer=customer)
+        cart, _ = Cart.objects.get_or_create(customer=customer)
         context['cart'] = cart
         context['item_count'] = cart.items.aggregate(total=Sum('quantity'))['total'] or 0
         return context
 
 
+@login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     customer = get_object_or_404(Customer, user=request.user)
-    cart, created = Cart.objects.get_or_create(customer=customer)
-    
+    cart, _ = Cart.objects.get_or_create(customer=customer)
+
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     if not created:
         cart_item.quantity += 1
         cart_item.save()
-    
-    # Get current amount of item in cart
+
     item_count = cart.items.aggregate(total=Sum('quantity'))['total'] or 0
 
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return JsonResponse({'item_count': item_count})
 
-    # Fallback for non-AJAX requests
     return redirect(request.META.get('HTTP_REFERER', '/'))
+
 
 @login_required
 def cart_detail(request):
     customer = get_object_or_404(Customer, user=request.user)
-    cart, created = Cart.objects.get_or_create(customer=customer)
+    cart, _ = Cart.objects.get_or_create(customer=customer)
     return render(request, 'cart_detail.html', {'cart': cart})
+
 
 @require_POST
 @login_required
@@ -85,6 +84,7 @@ def decrease_cart_item(request, item_id):
         'quantity': quantity,
         'item_count': item_count,
     })
+
 
 @login_required
 def get_cart_item_count(request):
