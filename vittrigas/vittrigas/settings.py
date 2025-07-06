a5dc86a0ed6080fda0971c9aafaa322cc5dbedba
RpnAndri <andri.crisostomo@gmail.com>
2025-07-07 01:38:37 +0800

    asdf

--- vittrigas/vittrigas/settings.py
+++ vittrigas/vittrigas/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")  # or "static" if you prefer
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")  # or "static" if you prefer
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
