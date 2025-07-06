dc2aab3b09ce6d7bba288ddb8b0c03a45a6bb3bc
RpnAndri <andri.crisostomo@gmail.com>
2025-07-07 00:45:30 +0800

    debug

--- vittrigas/vittrigas/wsgi.py
+++ vittrigas/vittrigas/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+print("✅ Hello from Django WSGI")
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vittrigas.settings')
 
 application = get_wsgi_application()
